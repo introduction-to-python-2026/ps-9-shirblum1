b75724ca20fdd918cac1cf4597f4e50b6840d359
shirblum1 <shirblum1@gmail.com>
2025-12-27 22:52:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,18 @@
 import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
 import joblib
 
-from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
+# load data
+df = pd.read_csv("parkinsons.csv")
 
-# import config
-from config import features, path
-
-
-# Load dataset
-url = "https://archive.ics.uci.edu/ml/machine-learning-databases/parkinsons/parkinsons.data"
-df = pd.read_csv(url)
-
-X = df[features]
+# select features and target
+X = df[["MDVP:Fo(Hz)", "HNR"]]
 y = df["status"]
 
-# Split data
-X_train, X_test, y_train, y_test = train_test_split(
-    X,
-    y,
-    test_size=0.25,
-    random_state=42
-)
-
-# Train model
+# train model
 model = KNeighborsClassifier(n_neighbors=7)
-model.fit(X_train, y_train)
+model.fit(X, y)
 
-# Evaluate
-y_pred = model.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred)
-print(accuracy)
-
-# Save model
-joblib.dump(model, path)
+# save model
+joblib.dump(model, "parkinson_model.joblib")
 
